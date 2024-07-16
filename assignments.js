5dd9bfd84b672662116a3cbcd9d5b28ad1e8c1ab
Bianka Csitari <bianka.csitari@ki-c02g5asvq6l5.user.ki.se>
2024-07-16 15:26:44 +0200

    initial commit

---
+++ assignments.js
@@ -0,0 +1,9 @@
+let myCountry = "Hungary";
+let myContinent = "Europe";
+let populationOfHungary = "9600000"
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(populationOfHungary);
+
+
