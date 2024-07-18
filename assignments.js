0058686a28e68ace0344374721241eb71a03755a
Bianka Csitari <bia.csitari@gmail.com>
2024-07-18 13:51:20 +0200

    Updates

--- assignments.js
+++ assignments.js
@@ -1,9 +1,39 @@
+//1. Values and variables
+/*
 let myCountry = "Hungary";
 let myContinent = "Europe";
-let populationOfHungary = "9600000"
+let populationOfHungary = 9600000
 
 console.log(myCountry);
 console.log(myContinent);
 console.log(populationOfHungary);
+
+//2. Data type
+
+let isIsland = false;
+let language;
+
+//boolean
+console.log(typeof isIsland);
+//number
+console.log(typeof populationOfHungary);
+//string
+console.log(typeof myCountry);
+//undefined
+console.log(typeof language);
+*/
+
+//3. Let, const and var
+
+const languageOfMyCountry = "Hungarian";
+console.log(languageOfMyCountry);
+
+let NameOfWorkplace = "Karolinska Institute";
+NameOfWorkplace = "New job";
+console.log(NameOfWorkplace);
+
+
+
+
 
 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -11,6 +12,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+//Variable name conventions
 let jonas_matilda = "JM";
 let _function = 27;
 
@@ -24,6 +26,60 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+*/
+
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+//const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Csitari";
+console.log(lastName);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
