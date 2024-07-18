f74d883c071f1160962bc3b8b15c2ba6fea11b07
Bianka Csitari <bia.csitari@gmail.com>
2024-07-18 17:59:11 +0200

    Implemented new features

--- assignments.js
+++ assignments.js
@@ -1,12 +1,12 @@
 //1. Values and variables
-/*
-let myCountry = "Hungary";
-let myContinent = "Europe";
-let populationOfHungary = 9600000
 
-console.log(myCountry);
-console.log(myContinent);
-console.log(populationOfHungary);
+let country = "Hungary";
+let continent = "Europe";
+let population = 9600000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
 
 //2. Data type
 
@@ -16,21 +16,56 @@ let language;
 //boolean
 console.log(typeof isIsland);
 //number
-console.log(typeof populationOfHungary);
+console.log(typeof population);
 //string
-console.log(typeof myCountry);
+console.log(typeof country);
 //undefined
 console.log(typeof language);
-*/
+
 
 //3. Let, const and var
 
-const languageOfMyCountry = "Hungarian";
-console.log(languageOfMyCountry);
+const languageOfHungary = "Hungarian";
+console.log(languageOfHungary);
 
 let NameOfWorkplace = "Karolinska Institute";
 NameOfWorkplace = "New job";
 console.log(NameOfWorkplace);
+
+
+//4. Basic operators
+
+console.log(population / 2); //4800000 people
+
+population++; //Increasing the population by 1
+console.log(population);
+
+let populationOfFinland = 6000000;
+
+console.log(population > populationOfFinland); //Does my country have more people than Finland? True
+
+const averagePopulation = 33000000;
+
+console.log(population < averagePopulation); //Does my country have less people than average? True
+
+const description1 = country + " is in " + continent + ", and its " + population + " people speak " + languageOfHungary;
+
+console.log(description1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- script.js
+++ script.js
@@ -64,7 +64,48 @@ lastName = "Csitari";
 console.log(lastName);
 */
 
-//Comment 
+// Maths operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//  2 ** 3 means 2 to the power of 3 = 2 + 2 + 2
+
+const firstName = "Bianka";
+const lastName = "Csitari";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; // x = 15 + 10
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
