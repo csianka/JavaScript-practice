33d3a6e7597eaff3f2b3919bfd4e762b3e8b81a9
Bianka Csitari <bia.csitari@gmail.com>
2024-08-14 14:15:28 +0200

    Conditional operator

--- assignments.js
+++ assignments.js
@@ -113,8 +113,32 @@ else {
  * Wolf changes end
  */
 
+//The switch Statement
 
+/*
+const language = "chinese"
 
+switch (language) {
+    case "chinese":
+    case "mandarin":
+        console.log("MOST number of native speakers!");
+        break;
+    case "spanish":
+        console.log("2nd place in number of nateive speakers");
+        break;
+    case "english":
+        console.log("3rd place");
+        break;
+    case "hindi":
+        console.log("Number 4");
+        break;
+    case "arabic":
+        console.log("5th most spoken language");
+        break;
+    default:
+        console.log("Great lanuage too.");
+}
+*/
 
 
 
--- script.js
+++ script.js
@@ -252,7 +252,7 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
     console.log("Someone else should drive...");
 }
 */
-
+/*
 const day = "monday";
 
 switch (day) {
@@ -278,73 +278,61 @@ switch (day) {
         console.log("Not a valid day!");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const travel = "december"
+
+switch (travel) {
+    case "september":
+        console.log("Poland");
+        break;
+    case "october":
+        console.log("Italy");
+        break;
+    case "november":
+        console.log("Canary Island");
+        break;
+    case "december":
+        console.log("Denmark");
+        break;
+    default:
+        console.log("No travelling for now");
+}
+
+if (day === "monday") {
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+    console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+    console.log("Write code examples");
+} else if (day === "friday") {
+    console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+    console.log("Enjoy the weekend");
+} else {
+    console.log("Not a valid day!");
+}
+*/
+/*
+if (23 > 10) {
+    const str = "23 is bigger";
+}
+
+console.log(`I’m ${2037 - 1992} years old.`)
+*/
+/*
+const age = 23;
+age >= 18 ? console.log("I like to drink wine") : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = "wine";
+} else {
+    drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+*/
